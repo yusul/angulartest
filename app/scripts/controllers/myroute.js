9107f0c02e506d81f4913169bd9162a572ca8a9c
yusul <yusul@naver.com>
2015-01-03 12:45:37 +0900

    initial commit

---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angulartestApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the angulartestApp
+ */
+angular.module('angulartestApp')
+  .controller('MyrouteCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
